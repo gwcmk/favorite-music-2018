489e6feefe942537ccb1b2acacd2b502f5b82070
gwcmk <gregwcmkim@gmail.com>
2018-12-31 12:12:29 -0600

    Second draft

---
+++ js/script.js
@@ -0,0 +1,66 @@
+// lmao too lazy to alphabetically sort this
+var songs = [
+  "Virtual Self - Ghost Voices",
+  "Migos - Stir Fry",
+  "Diplo (feat. MO & Goldlink) - Get It Right [Remix]",
+  "Calvin Harris, Dua Lipa - One Kiss",
+  "Cardi B, Bad Bunny & J Balvin - I Like It",
+  "Khalid, 6LACK & Ty Dolla $ign - OTW",
+  "Goldlink (feat. Miguel) - Got Friends",
+  "Drake - In My Feelings",
+  "Meek Mill - 1am",
+  "Channel Tres - Jet Black",
+  "03 Greedo (feat. Lil Uzi Vert) - Never Bend (Remix)",
+  "Rico Nasty - Countin’ Up",
+  "Travis Scott (feat. Gunna) - Yosemite",
+  "Travis Scott (feat. Drake) - Sicko Mode",
+  "Ariana Grande - breathin",
+  "Tyga (feat. Offset) - Taste",
+  "Nao (feat. SiR) - Make It Out Alive",
+  "YBN Nahmir & YBN Almighty Jay - Porsches in the Rain",
+  "Troye Sivan - Bloom",
+  "Young Thug (feat. Jaden Smith) - Sin",
+  "Sheck Wes - Gmail",
+  "Future & Juice WRLD (feat. Young Thug) - Red Bentley",
+  "K/DA - POP/STARS",
+  "Smino - L.M.F.",
+  "KEY! & Kenny Beats - Demolition 1 + 2",
+  "Meek Mill (feat. Future, Roddy Ricch, and Young Thug) - Splash Warning",
+  "Meek Mill (feat. Drake) - Going Bad",
+  "Meek Mill (feat. 21 Savage) - Pay You Back",
+  "Rosalia - Malamente",
+  "Joey Bada$$ - King Of The Jungle",
+  "YoungBoy Never Broke Again (feat. Lil Uzi Vert) - What You Know",
+  "Bad Bunny - Otra Noche en Miami",
+  "Bad Bunny - Solo de Mí",
+  "Bad Bunny (feat. Drake) - MIA",
+  "J.I.D. - Working Out",
+  "J.I.D. - 151 Rum",
+  "A$AP Rocky (feat. Skepta) - Praise The Lord (Da Shine)",
+  "Niña Dioz (feat. Lido Pimienta & Ceci Bastida) - Tambalea",
+  "J. Balvin - Ambiente",
+  "Nicky Jam & J Balvin (feat. Ozuna & Maluma) - X (Remix)",
+  "SOB X RBE - Paramedic!",
+  "SOB X RBE - Always",
+  "SOB X RBE - Anti Social",
+  "21 Savage - a lot",
+  "21 Savage - out for the night",
+  "21 Savage - 1.5",
+  "IU - Bbibbi",
+  "Young Thug (feat. Gunna & Lil Baby) - Chanel",
+  "Metro Boomin (feat. Gunna) - Space Cadet",
+  "Lil Baby (feat. Gunna & Lil Uzi Vert) - Life Goes On"
+]
+var sortedSongs = songs.sort();
+
+(function () {
+  var extras = document.querySelector('.extras');
+  var songElement, songTitleElement;
+  sortedSongs.forEach(function (title) {
+    songElement = document.createElement('div');
+    songTitleElement = document.createElement('small');
+    songTitleElement.textContent = title;
+    songElement.append(songTitleElement);
+    extras.append(songElement);
+  })
+})();
